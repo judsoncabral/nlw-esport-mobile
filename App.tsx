6b14033264de339f08b2fadbade8897f64d87a5e
Judson Cabral de Melo <judson.cabral.010@ufrn.edu.br>
2022-09-14 20:56:22 -0300

    chore: base do projeto do nlw-esports mobile

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Background } from "./src/components/Background";
+import { StatusBar } from "react-native";
+export default function App() {
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      ></StatusBar>
+    </Background>
+  );
+}
